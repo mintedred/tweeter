d173f0c8e0a4ae9da43ee31fee63dbbe711bb34b
Corina Wong <hi@corinawong.com>
2017-10-20 22:39:18 +0000

    Use SASS

--- server/index.js
+++ server/index.js
@@ -5,7 +5,20 @@
 const PORT          = 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
-const app           = express();
+const sassMiddleware = require('node-sass-middleware');
+
+const MongoClient = require("mongodb").MongoClient;
+const MONGODB_URI = "mongodb://localhost:27017/tweeter";
+
+const app = express();
+
+app.use(sassMiddleware({
+  src: '../stylesheets',
+  dest: '../public/css',
+  debug: true,
+  indentedSyntax : true,
+  outputStyle: 'compressed'
+}))
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -28,11 +41,6 @@ const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 
 // Mount the tweets routes at the "/tweets" path prefix:
 app.use("/tweets", tweetsRoutes);
-
-
-
-
-
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
