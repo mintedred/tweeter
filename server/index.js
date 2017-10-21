2661533594fd03dd2ad255be7250624d73423b3d
Corina Wong <hi@corinawong.com>
2017-10-21 15:44:19 +0000

    Make site responsive

--- server/index.js
+++ server/index.js
@@ -1,7 +1,5 @@
 "use strict";
 
-// Basic express setup:
-
 const PORT          = 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
@@ -23,19 +21,14 @@ app.use(sassMiddleware({
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-
 MongoClient.connect(MONGODB_URI, (err, db) => {
   if (err) {
     console.error(`Failed to connect: ${MONGODB_URI}`);
     throw err;
   }
-  
   console.log(`Connected to mongodb: ${MONGODB_URI}`);
 
   // The `data-helpers` module provides an interface to the database of tweets.
-  // This simple interface layer has a big benefit: we could switch out the
-  // actual database it uses and see little to no changes elsewhere in the code
-  //
   // Because it exports a function that expects the `db` as a parameter, we can
   // require it and pass the `db` parameter immediately:
   const DataHelpers = require("./lib/data-helpers.js")(db);
